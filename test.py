f2f7b79f456a5438474d59e761d899aab320e5c7
JonasUnruh <jonas.unruh@t-online.de>
2024-06-08 09:47:55 +0200

    added correct geojson

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 df = pd.read_csv("./data/rent_inc.csv")
-with open("./data/BEZIRKSGRENZEOGD.json", "r") as f:
+with open("./data/bezirke_95_geo.json", "r") as f:
     geojson = json.loads(f.read())
 
 app = Dash()
@@ -22,8 +22,15 @@ app.layout = [
 def update_graph(test):
     fig = px.choropleth_mapbox(
         df, geojson=geojson, color="rent",
-        locations="district", featureidkey="properties.DISTRICT_CODE",
-        mapbox_style="carto-positron")
+        locations="district", featureidkey="properties.iso",
+        mapbox_style="carto-positron",
+        color_continuous_scale = px.colors.sequential.Viridis_r,
+        zoom = 9.5,
+        animation_frame = "year",
+        width = 750,
+        height = 550,
+        center = {"lat": 48.210033, "lon": 16.363449},
+        opacity = 0.3)
     fig.update_layout(
         margin={"r":0,"t":0,"l":0,"b":0})
 
